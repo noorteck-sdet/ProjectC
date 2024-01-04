559df811aa2d92abbdc6e2aba53bc64746571db1
noort <noort@DESKTOP-KKE0VB6>
2024-01-03 21:03:22 -0500

    Initial Commit

---
+++ src/c/DemoC.java
@@ -0,0 +1,11 @@
+package c;
+
+public class DemoC {
+	public static void main(String[] args) {
+		System.out.println("Wed");
+		
+		System.out.println("Batch 11");
+		System.out.println("Batch 11 Git Lecture");
+
+	}
+}
